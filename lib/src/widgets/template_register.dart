aa51091be34b0b64a91b17c93aa15358504dc07c
Hashan Ranasinghe <73054932+hashanranasinghe@users.noreply.github.com>
2025-01-29 00:56:18 +0530

    add inputfield

--- lib/src/widgets/template_register.dart
+++ lib/src/widgets/template_register.dart
@@ -1,8 +1,12 @@
 class TemplateRegister {
   final Map<String, Map<String, String>> templates = {
     'button': {
-      'path': 'lib/src/widgets/button_template.dart',
+      'path': 'lib/src/widgets/buttons/button_template.dart',
       'folder': 'buttons'
+    },
+    'inputfield': {
+      'path': 'lib/src/widgets/inputfields/basic_inputfield.dart',
+      'folder': 'inputfields'
     },
     'dummy': {
       'path': 'lib/src/widgets/dummy_template.dart',
